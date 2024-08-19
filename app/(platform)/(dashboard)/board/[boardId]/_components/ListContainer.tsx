c91ec51f06143d75201c9dc09c6300dfdf611ac2
dhruvsuri789 <dhruvsuri789@gmail.com>
2024-08-19 17:55:50 +0530

    Added update lists functionality

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListContainer.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListContainer.tsx
@@ -2,6 +2,8 @@
 
 import { ListWithCards } from "@/types";
 import ListForm from "./ListForm";
+import { useEffect, useState } from "react";
+import ListItem from "./ListItem";
 
 interface ListContainerProps {
   data: ListWithCards[];
@@ -9,8 +11,23 @@ interface ListContainerProps {
 }
 
 function ListContainer({ data, boardId }: ListContainerProps) {
+  // For drag and drop we need a source of truth which we will modify locally and then in the database
+  // We could directly do it in the database and skip this local part, but it will have bad user experience
+  // Because with drag and drop its important that we have a working optimistic mutations like when you like something.
+  // Optimistic mutations : We know it will happen so we show the result before we actually mutate in the database
+  // When it comes to drag and drop its a really bad experience to have to wait for a second
+  const [orderedData, setOrderedData] = useState(data);
+
+  // Whenever data updates, we will pass in the new data.
+  useEffect(() => {
+    setOrderedData(data);
+  }, [data]);
+
   return (
-    <ol>
+    <ol className="flex gap-x-3 h-full">
+      {orderedData.map((list, index) => (
+        <ListItem key={list.id} data={list} index={index} />
+      ))}
       <ListForm />
       <div className="flex-shrink-0 w-1"></div>
     </ol>
---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListHeader.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { useState, useRef, ElementRef } from "react";
+import { List } from "@prisma/client";
+import { useEventListener } from "usehooks-ts";
+import FormInput from "@/components/form/FormInput";
+import { useAction } from "@/hooks/useAction";
+import { updateList } from "@/actions/update-list";
+import { toast } from "sonner";
+
+interface ListHeaderProps {
+  data: List;
+}
+
+function ListHeader({ data }: ListHeaderProps) {
+  const [title, setTitle] = useState(data.title);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const inputRef = useRef<ElementRef<"input">>(null);
+  const formRef = useRef<ElementRef<"form">>(null);
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    // Not the best solution but kind of like a hack
+    // You can explore flushSync, but react docs mentions that it should only be used as the last resort
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.select();
+    });
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const { execute } = useAction(updateList, {
+    onSuccess: (data) => {
+      toast.success(`Renamed to "${data.title}"`);
+      setTitle(data.title);
+      disableEditing();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const handleSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    const id = formData.get("id") as string;
+    const boardId = formData.get("boardId") as string;
+
+    if (title === data.title) return disableEditing();
+
+    execute({ id, title, boardId });
+  };
+
+  const onBlur = () => {
+    formRef.current?.requestSubmit();
+  };
+
+  const onKeyDown = (event: KeyboardEvent) => {
+    if (event.key === "Escape") {
+      formRef.current?.requestSubmit();
+    }
+  };
+
+  useEventListener("keydown", onKeyDown);
+
+  return (
+    <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2">
+      {isEditing ? (
+        <form ref={formRef} action={handleSubmit} className="flex-1 px-[2px]">
+          <input hidden id="id" name="id" value={data.id} />
+          <input hidden id="boardId" name="boardId" value={data.boardId} />
+          <FormInput
+            ref={inputRef}
+            id="title"
+            placeholder="Enter list title..."
+            defaultValue={title}
+            onBlur={onBlur}
+            className="text-sm px-[7px] py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition truncate bg-transparent focus:bg-white"
+          />
+          <button type="submit" hidden />
+        </form>
+      ) : (
+        <div
+          onClick={enableEditing}
+          className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent"
+        >
+          {title}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ListHeader;
